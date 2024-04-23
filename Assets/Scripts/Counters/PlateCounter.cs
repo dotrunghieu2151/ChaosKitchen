1392642899ecec8c3ff43a60411f13fbb0bf4b57
hieu.do <hieu.do@kiaisoft.com>
2024-04-23 17:33:56 +0700

    Adding plateCounter

---
+++ Assets/Scripts/Counters/PlateCounter.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCounter : BaseCounter
+{
+    [SerializeField] private KitchenObjectSO _kitchenObjectSO;
+
+    public event EventHandler OnPlayerGrabbedObject;
+
+    public override bool CanInteract(IKitchenObjectParent parent)
+    {
+        return !parent.HasKitchenObject();
+    }
+
+
+    public override void Interact(IKitchenObjectParent parent)
+    {
+        if (!parent.HasKitchenObject())
+        {
+            KitchenObject.SpawnKitchenObject(_kitchenObjectSO, parent);
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+
+}
